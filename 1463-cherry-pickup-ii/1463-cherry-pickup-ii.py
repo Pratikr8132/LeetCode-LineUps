8aadbfa45777cd4ef498f0154361ec855bb550d3
Pratik Rathod <89502694+Pratikr8132@users.noreply.github.com>
2024-02-11 22:24:25 +0530

    Time: 335 ms (99.75%), Space: 16.9 MB (99.75%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,43 +1,16 @@
-class Solution(object):
-    def cherryPickup(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        if len(grid) == 0:
-            return 0
-        elif len(grid[0]) == 1:
-            return grid[0][0] + grid[0][-1]
-        
-        dirs = [(0,0),(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1)]
-        
-        M, N = len(grid), len(grid[0])
-        
-        old = [[0 for _ in range(N+2)] for _ in range(N+2)]
-        
-        
-        engrid = [[0 for _ in range(N+2)] for _ in range(M)]
-        for i in range(M):
-            for j in range(1, N+1):
-                engrid[i][j] = grid[i][j-1]
-        
-        old[1][N] = grid[0][0] + grid[0][-1]
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        n_cols = len(grid[0])
+        best = [[0] * (n_cols + 1) for _ in range(n_cols + 1)]
+        next_best = copy.deepcopy(best)
+        for i in reversed(range(len(grid))):
+            for left in range(min(i + 1, n_cols - 1)):
+                for right in range(max(left + 1, n_cols - i - 1), n_cols):
+                    next_best[left][right] = grid[i][left] + grid[i][right] + max(
+                        best[left + i][right + j] for i in [-1, 0, 1] for j in [-1, 0, 1]
+                        if left + i < right + j
+                    )
 
-        tmp = [[0 for _ in range(N+2)]  for _ in range(N+2)]
-        for i in range(M-1):
-            new = [[0 for _ in range(N+2)] for _ in range(N+2)]
-            for j1 in range(1, N+1):
-                for j2 in range(1, N+1):
-                    if old[j1][j2] or (i == 0 and grid[0][0] + grid[0][-1] == 0):
-                        for d1, d2 in dirs:
-                            nxt_j1, nxt_j2 = j1 + d1, j2 + d2
-                            if nxt_j1 == 0 or nxt_j2 == N+1 or nxt_j2 == 0 or nxt_j1 == N+1:
-                                continue
-                            tmp = engrid[i+1][nxt_j1] + engrid[i+1][nxt_j2] if nxt_j1 != nxt_j2 else engrid[i+1][nxt_j1]
-                            new[nxt_j1][nxt_j2] = max(new[nxt_j1][nxt_j2], old[j1][j2] + tmp)
-            old = new
-                        
-        res = 0
-        for j1 in range(1, N+1):
-            res = max(res, max(old[j1]))
-        return res
+            best, next_best = next_best, best 
+
+        return best[0][n_cols - 1]
